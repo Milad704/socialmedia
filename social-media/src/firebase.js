39c6aa9a1ed8e9847d9ad2805171f4c8e407ae3d
Milad704 <miladgobbo@gmail.com>
2025-05-28 09:29:54 -0700

    f

---
+++ social-media/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDc2GXUjj2pt9D7cmsKGCYFbQeim7-HTpU",
+  authDomain: "socialmediaapp-170ea.firebaseapp.com",
+  projectId: "socialmediaapp-170ea",
+  storageBucket: "socialmediaapp-170ea.firebasestorage.app",
+  messagingSenderId: "874225109606",
+  appId: "1:874225109606:web:d042112d8a9ba726c27ba7",
+  measurementId: "G-SXRNB6ZM3P"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
