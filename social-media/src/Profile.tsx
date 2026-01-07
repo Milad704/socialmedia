391b7e25c9066190a9a4b40404a4f5eb6b4b66b8
Milad704 <miladgobbo@gmail.com>
2026-01-07 12:57:02 -0800

    istuff

--- social-media/src/Profile.tsx
+++ social-media/src/Profile.tsx
@@ -13,7 +13,6 @@ export default function Profile({currentUser, currentImg}: Props) {
     return (
         <><h4>your username is {currentUser}</h4>
         <img src={currentImg} alt="Profile" style={{ width: "120px", height: "120px" }}/>  
-
         </>
     )
 
